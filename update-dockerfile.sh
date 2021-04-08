c8388493ae6a1a8bc4ad8bc9dfe054421771e84d
pestotoast <adrian@pestotoast.de>
2021-04-08 17:44:21 +0000

    Update 'update-dockerfile.sh'

--- update-dockerfile.sh
+++ update-dockerfile.sh
@@ -1,2 +1,3 @@
 sed -i s/php:7.4-fpm-alpine3.13/php:8-fpm-alpine/g docker/21.0/fpm-alpine/Dockerfile
 sed -i 's/pecl install imagick-3.4.4;/mkdir -p \/usr\/src\/php\/ext\/imagick; curl -fsSL https:\/\/github.com\/Imagick\/imagick\/archive\/refs\/heads\/master.tar.gz \| tar xvz -C \"\/usr\/src\/php\/ext\/imagick\" --strip 1; docker-php-ext-install imagick;/g' docker/21.0/fpm-alpine/Dockerfile
+sed -i 's/ENV NEXTCLOUD_VERSION 21.0.0/ENV NEXTCLOUD_VERSION 21.0.1/g' docker/21.0/fpm-alpine/Dockerfile
